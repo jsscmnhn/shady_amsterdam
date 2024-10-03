4eea391ce5054f330960f6a20290666ad2c27dc5
Ganbusier <953133083@qq.com>
2024-10-03 14:57:56 +0200

    update

--- cool_place/include/cool_space.py
+++ cool_place/include/cool_space.py
@@ -3,14 +3,16 @@ import rasterio
 from rasterio.mask import mask
 from scipy.ndimage import label
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class CoolSpace:
     def __init__(self, data: gpd.geodataframe) -> None:
         self.data = data
-        self.data['clipped'] = None
-        self.data['shade_avg'] = None
-        self.data['shade_areas'] = None
+        self.data["clipped"] = None
+        self.data["shadeAvg"] = None
+        self.data["shadeAreas"] = None
+        self.data["shadeGeoms"] = None
+        self.data["qualiGeoms"] = None
 
     def clip(self, to_clip: gpd.geodataframe, clipper: gpd.geodataframe, how='difference') -> None:
         if how not in ['difference', 'intersection', 'union', 'symmetric_difference']:
@@ -23,8 +25,8 @@ class CoolSpace:
         self.data['clipped'] = clipped.geometry
 
     def calculate_shade(self, raster: rasterio.io.DatasetReader, area_thres=200, use_clip=False) -> None:
-        self.data['shade_avg'] = None
-        self.data['shade_areas'] = None
+        self.data["shadeAvg"] = None
+        self.data["shadeAreas"] = None
 
         geometries = self.data['clipped'] if use_clip and 'clipped' in self.data else self.data.geometry
 
@@ -33,11 +35,12 @@ class CoolSpace:
             out_image, out_transform = mask(raster, geom_geojson, crop=True)
             out_image = out_image[0]  # Assume shade value is stored in the first band
 
-            # Calculate average shading value for this clipped area
+            # Calculate average shading value for this clipped area (just a test)
             valid_data = out_image[out_image >= 0]
             avg = valid_data.mean() if valid_data.size > 0 else 0
 
             # Calculate continuous shaded area which are larger than the area threshold
+            # This step should put another threshold for each pixel, right now is >= 0
             labeled_array, num_features = label(out_image >= 0)
             pixel_size = out_transform[0] * (-out_transform[4])  # x_length * y_length for each pixel
             pixel_areas = []
@@ -47,44 +50,36 @@ class CoolSpace:
                     pixel_areas.append(region_area)
 
             # Store the results in the GeoDataFrame columns
-            self.data.at[idx, 'shade_avg'] = avg
-            self.data.at[idx, 'shade_areas'] = pixel_areas
+            self.data.at[idx, "shadeAvg"]: float = avg
+            self.data.at[idx, "shadeAreas"]: list = pixel_areas
 
-    def is_all_qualified(self, ratio: float) -> bool:
-        if self.data['shade_areas'].isnull().all():
-            return False
-
-        for idx, row in self.data.iterrows():
-            total_area = row['geometry'].area  # Get total area of the current clipped geometry
-            if not row['shade_areas']:
-                return False  # If there are no shade areas, return False
-
-            max_shade_area = max(row['shade_areas'])  # Get the largest shade area
-            if max_shade_area / total_area < ratio:
-                return False  # If the largest shade area is less than the ratio, return False
-
-        return True  # If all areas pass the ratio test, return True
-
-    def is_qualified(self, index: int, ratio: float) -> bool:
-        if index >= len(self.data) or index < 0:
-            raise IndexError(f"Invalid index: {index}. Must be between 0 and {len(self.data) - 1}.")
-
-        row = self.data.iloc[index]
-        total_area = row['geometry'].area
-        if not row['shade_areas']:
-            return False
-
-        max_shade_area = max(row['shade_areas'])
-        return max_shade_area / total_area >= ratio
-
-    def get_qualified_geometries(self, min_shade_area=200) -> gpd.GeoDataFrame:
+    def get_qualified_shaded_geometries(self, min_shade_area=200) -> None:
         """
-        return geometries that have shaded areas larger than 200m2
+        get geometries that have shaded areas larger than 200m2
         """
-        qualified_geometries = self.data[self.data['shade_areas'].apply(
-            lambda x: any(area >= min_shade_area for area in x) if x else False)]
-        return qualified_geometries
+        self.data["qualiGeoms"]: bool = self.data["shadeAreas"].apply(
+            lambda x: any(area >= min_shade_area for area in x) if x else False
+        )
 
+        if self.data["qualiGeoms"].any():
+            print(f"There are {self.data['qualiGeoms'].sum()}/{self.data.shape[0]} qualified geometries.")
+        else:
+            print("No qualified geometries found.")
+
+    def get_qualified_shape_geometries(self, ratio_thres=0.35, use_clip=False) -> None:
+        """
+        get geometries that have the ratio of area to perimeter > 0.35
+        """
+        geometries = self.data['clipped'] if use_clip and 'clipped' in self.data else self.data.geometry
+        self.data["clipped_perimeter"] = geometries.length
+        self.data["clipped_area"] = geometries.area
+        self.data["clipped_perimeter_area_ratio"] = self.data["clipped_area"] / self.data["clipped_perimeter"]
+        self.data["qualiGeoms"]: bool = self.data["clipped_perimeter_area_ratio"] > ratio_thres
+
+        if self.data["qualiGeoms"].any():
+            print(f"There are {self.data['qualiGeoms'].sum()}/{self.data.shape[0]} qualified geometries.")
+        else:
+            print("No qualified geometries found.")
 
 
 if __name__ == '__main__':
@@ -95,5 +90,5 @@ if __name__ == '__main__':
     filepath_win = directory_win + filename
 
     coolSpace = CoolSpace(gpd.read_file(filepath_win))
-    print(coolSpace.data.head(5))
+    coolSpace.get_qualified_shaded_geometries()
 
