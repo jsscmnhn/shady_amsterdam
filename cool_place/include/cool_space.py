e91294c36ce379805260804dc3a1557b9ba96acf
Ganbusier <953133083@qq.com>
2024-10-03 22:59:11 +0200

    Update cool_space.py

--- cool_place/include/cool_space.py
+++ cool_place/include/cool_space.py
@@ -1,7 +1,9 @@
 import geopandas as gpd
 import rasterio
 from rasterio.mask import mask
+from rasterio.features import shapes
 from scipy.ndimage import label
+from shapely.geometry import shape
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,12 +12,9 @@ class CoolSpace:
     def __init__(self, data: gpd.geodataframe) -> None:
         self.data = data
         self.data["clipped"] = None
-        self.data["shadeAvg"] = None
-        self.data["shadeAreas"] = None
-        self.data["shadeGeoms"] = None
         self.data["shade_qualified"] = None
         self.data["shape_qualified"] = None
-        self.data["qualiGeoms"] = None
+        self.data["geoms_qualified"] = None
 
     def clip(self, to_clip: gpd.geodataframe, clipper: gpd.geodataframe, how='difference') -> None:
         if how not in ['difference', 'intersection', 'union', 'symmetric_difference']:
@@ -27,9 +26,14 @@ class CoolSpace:
         clipped = gpd.overlay(to_clip, clipper, how=how)
         self.data['clipped'] = clipped.geometry
 
-    def calculate_shade(self, raster: rasterio.io.DatasetReader, area_thres=200, use_clip=False) -> None:
-        self.data["shadeAvg"] = None
-        self.data["shadeAreas"] = None
+    def calculate_shade(self, rasters: list[rasterio.io.DatasetReader], area_thres=200, use_clip=False) -> None:
+        """
+        input a series of rasters, calculate each raster's average shade, areas of shade, and store corresponding
+        shade pixels.
+        :param rasters: input list of rasters
+        :param area_thres: minimum threshold of shade area, by default is 200m2
+        :param use_clip: use clipped geometry or not, if there is no clipped geometry, original will be used.
+        """
 
         if use_clip and 'clipped' in self.data:
             geometries = self.data['clipped']
@@ -37,33 +41,54 @@ class CoolSpace:
             print("No clipped geometry, default geometry will be used.")
             geometries = self.data.geometry
 
-        for idx, geom in geometries.items():
-            geom_geojson = [geom.__geo_interface__]  # Convert single geometry to geojson format
-            out_image, out_transform = mask(raster, geom_geojson, crop=True)
-            out_image = out_image[0]  # Assume shade value is stored in the first band
+        # same calculation for each raster
+        for raster_idx, raster in enumerate(rasters):
+            print(f"Processing raster {raster_idx + 1}/{len(rasters)}")
 
-            # Calculate average shading value for this clipped area (just a test)
-            valid_data = out_image[out_image >= 0]
-            avg = valid_data.mean() if valid_data.size > 0 else 0
+            # initialize attributes for current raster
+            self.data[f"shadeAvg{raster_idx}"] = None
+            self.data[f"shadeArea{raster_idx}"] = None
+            self.data[f"shadeGeom{raster_idx}"] = None
 
-            # Calculate continuous shaded area which are larger than the area threshold
-            # This step should put another threshold for each pixel, right now is >= 0
-            labeled_array, num_features = label(out_image >= 0)
-            pixel_size = out_transform[0] * (-out_transform[4])  # x_length * y_length for each pixel
-            pixel_areas = []
-            for region_label in range(1, num_features + 1):
-                region_area = np.sum(labeled_array == region_label) * pixel_size
-                if region_area >= area_thres:
-                    pixel_areas.append(region_area)
+            for idx, geom in geometries.items():
+                geom_geojson = [geom.__geo_interface__]  # transform geom to GeoJSON
+                out_image, out_transform = mask(raster, geom_geojson, crop=True)
+                out_image = out_image[0]  # assume shade value is stored in band 1
 
-            # Store the results in the GeoDataFrame columns
-            self.data.at[idx, "shadeAvg"]: float = avg
-            self.data.at[idx, "shadeAreas"]: list = pixel_areas
+                # calculate average shade value
+                valid_data = out_image[out_image >= 0]
+                avg = valid_data.mean() if valid_data.size > 0 else 0
+
+                # for all the pixels have shade value >= 0.5, calculate the continuous area
+                labeled_array, num_features = label(out_image >= 0.5)
+                pixel_size = out_transform[0] * (-out_transform[4])  # area of a pixel
+                pixel_areas = []
+                shade_polygons = []
+
+                # only select those continuous area larger than 200m2
+                for region_label in range(1, num_features + 1):
+                    region_area = np.sum(labeled_array == region_label) * pixel_size
+                    if region_area >= area_thres:
+                        pixel_areas.append(region_area)
+
+                        # create region mask (bool)
+                        region_mask = (labeled_array == region_label).astype(np.uint8)
+
+                        # use rasterio.features.shapes to transform the mask to geometry
+                        for geom, value in shapes(region_mask, transform=out_transform):
+                            if value == 1:  # reserve the "true" part
+                                shade_polygons.append(shape(geom))
+
+                # 将结果存储到 GeoDataFrame 的相应列中
+                self.data.at[idx, f"shadeAvg{raster_idx}"] = avg
+                self.data.at[idx, f"shadeArea{raster_idx}"] = pixel_areas
+                self.data.at[idx, f"shadeGeom{raster_idx}"] = shade_polygons
 
     def get_qualified_shaded_geometries(self, min_shade_area=200) -> None:
         """
         get geometries that have continuous shaded areas larger than 200m2
         """
+        self.data["shade_qualified"] = None
         self.data["shade_qualified"]: bool = self.data["shadeAreas"].apply(
             lambda x: any(area >= min_shade_area for area in x) if x else False
         )
@@ -101,8 +126,8 @@ class CoolSpace:
         if not self.data["shade_qualified"].any() or not self.data["shape_qualified"].any():
             raise ValueError("Please check shade and shape qualification first.")
 
-        self.data["qualiGeoms"] = None
-        self.data["qualiGeoms"] = self.data[self.data["shade_qualified"] and self.data["shape_qualified"]]
+        self.data["geoms_qualified"] = None
+        self.data["geoms_qualified"] = self.data[self.data["shade_qualified"] and self.data["shape_qualified"]]
 
 
 if __name__ == '__main__':
