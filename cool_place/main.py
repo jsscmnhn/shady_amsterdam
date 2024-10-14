efd5efbcfbb9e7d746f7f079a1f66a666b5c3949
Ganbusier <953133083@qq.com>
2024-10-14 18:49:47 +0200

    fix shade cover calculation

--- cool_place/main.py
+++ cool_place/main.py
@@ -1,7 +1,12 @@
-import geopandas
-import shapely
-
-import include.road_process
+from include.road_process import Road
+from include.cool_space import CoolSpace
+from include.building import Building
+import geopandas as gpd
+import numpy as np
+import rasterio
+import matplotlib.pyplot as plt
+import glob
+import os
 
 def print_hi(name):
     print(f'Hi, {name}')
