f97c6a9d7e7e7894802ad73a2039b95e62fef053
citrandina <150625410+citrandina@users.noreply.github.com>
2024-10-24 22:05:01 +0200

    eval

--- cool_place/include/evaluation.py
+++ cool_place/include/evaluation.py
@@ -10,24 +10,42 @@ import os
 from shapely.geometry import base
 from shapely import wkt
 import re
-import osmnx as ox
 from shapely.geometry import Point, shape
 import time
 from rich.progress import Progress
 
-
 class CoolEval:
     def __init__(self, cool_places: gpd.GeoDataFrame, buildings: gpd.GeoDataFrame, bench: gpd.GeoDataFrame,
                  heatrisk: gpd.GeoDataFrame, pet, search_buffer: float) -> None:
-        self.cool_places = cool_places  # This will hold the final output
+        """
+        Initialize the CoolEval object with the input datasets.
+
+        Parameters:
+        cool_places (GeoDataFrame): Cool places to be evaluated.
+        buildings (GeoDataFrame): Buildings dataset with attributes such as resident, elder_resi, and kid.
+        bench (GeoDataFrame): Dataset containing information about benches.
+        heatrisk (GeoDataFrame): Dataset containing heat risk information.
+        pet (str): Path to a raster dataset for PET evaluation.
+        search_buffer (float): Buffer distance for assigning buildings to cool places.
+        """
+        self.cool_places = cool_places
         self.buildings = buildings
         self.bench = bench
         self.heatrisk = heatrisk
         self.pet = pet
         self.search_buffer = search_buffer
-        self.eval_shades = []  # Collect evaluation results for each shade geometry column (sdgeom1, sdgeom2, etc.)
+        self.eval_shades = []
 
     def calculate_walking_shed(self):
+        """
+        Assign each building to the nearest cool place within a specified buffer distance.
+
+        The function computes the walking shed by creating a buffer around each cool place,
+        finding the buildings within that buffer, and assigning the nearest cool place ID to each building.
+
+        Returns:
+        GeoDataFrame: Updated buildings dataset with the assigned cool place ID.
+        """
         with Progress() as progress:
             task = progress.add_task("Calculate walking shed...", total=3)
             start_time = time.time()
@@ -37,7 +55,7 @@ class CoolEval:
             if self.cool_places.crs.is_geographic:
                 self.cool_places = self.cool_places.to_crs(epsg=28992)
 
-            buildings['c_id'] = None
+            self.buildings['c_id'] = None
             building_sindex = self.buildings.sindex
 
             for cool_place in self.cool_places.itertuples():
@@ -54,7 +72,7 @@ class CoolEval:
                         self.buildings.loc[
                             idx, 'c_id'] = cool_place.id
 
-            missing_cool_place = buildings['c_id'].isna().sum()
+            missing_cool_place = self.buildings['c_id'].isna().sum()
             if missing_cool_place > 0:
                 print(f"Warning: {missing_cool_place} buildings were not assigned to any cool place.")
 
@@ -62,11 +80,19 @@ class CoolEval:
             end_time = time.time()
             duration = end_time - start_time
             print(f"Calculating walking shed took {duration:.2f} seconds")
-            self.buildings.to_file("build_ws.shp")
 
             return self.buildings
 
     def evaluate_resident(self) -> gpd.GeoDataFrame:
+        """
+        Aggregate the number of residents, elderly residents, and children near each cool place.
+
+        This function sums up the resident-related attributes from buildings within a certain distance
+        to each cool place, then merges the totals back to the cool places dataset.
+
+        Returns:
+        GeoDataFrame: Cool places dataset with added columns for residents, elderly residents, and children.
+        """
         with Progress() as progress:
             task = progress.add_task("Evaluate resident...", total=4)
             start_time = time.time()
@@ -87,16 +113,26 @@ class CoolEval:
             self.cool_places = self.cool_places.merge(building_grouped, left_on='id', right_on='c_id', how='left')
             progress.advance(task)
 
-            self.cool_places.to_file("cp_ws.shp")
-            progress.advance(task)
 
             end_time = time.time()
             duration = end_time - start_time
             print(f"Calculating walking shed took {duration:.2f} seconds")
 
             return self.cool_places
-
     def evaluate_capacity(self, shade: gpd.GeoDataFrame, layer_name: str) -> gpd.GeoDataFrame:
+        """
+        Evaluate the capacity of shaded areas based on their size and nearby residents.
+
+        The function calculates the area of each shade polygon and determines how many people it can accommodate.
+        It then compares this capacity with the number of nearby residents and assigns a capacity status.
+
+        Parameters:
+        shade (GeoDataFrame): The shade dataset to evaluate.
+        layer_name (str): Name of the layer being evaluated.
+
+        Returns:
+        GeoDataFrame: Updated shade dataset with capacity-related attributes.
+        """
 
         shade['Area'] = shade['geometry'].area.round(2)
         shade['cap_area'] = shade.apply(
@@ -119,11 +155,23 @@ class CoolEval:
         #     axis=1
         # )
         return shade
-
     def evaluate_sfurniture(self, shade: gpd.GeoDataFrame, layer_name: str) -> gpd.GeoDataFrame:
+        """
+        Evaluate the availability of benches within shaded areas.
+
+        This function checks for the presence of benches in each shaded area by performing a spatial join
+        and counts the benches in each polygon.
+
+        Parameters:
+        shade (GeoDataFrame): The shade dataset to evaluate.
+        layer_name (str): Name of the layer being evaluated.
+
+        Returns:
+        GeoDataFrame: Updated shade dataset with bench availability information.
+        """
         start_time = time.time()
 
-        bench_join = gpd.sjoin(self.bench, shade, how="inner", predicate='intersects')
+        bench_join = gpd.sjoin(self.bench, shade, how="inner", predicate='intersects', lsuffix='_left', rsuffix='_right')
         count_benches = bench_join.groupby('id').size().reset_index(name='benches_count')
         shade = shade.merge(count_benches, on='id', how='left')
         shade['Benches'] = shade['benches_count'].fillna(0).apply(
@@ -135,9 +183,22 @@ class CoolEval:
         return shade
 
     def evaluate_heatrisk(self, shade: gpd.GeoDataFrame, layer_name: str) -> gpd.GeoDataFrame:
+        """
+        Evaluate the heat risk within shaded areas.
+
+        The function joins the shade dataset with the heat risk dataset and calculates the average heat risk
+        for each shaded area. It also classifies heat risk into five levels: "Lowest" to "Highest".
+
+        Parameters:
+        shade (GeoDataFrame): The shade dataset to evaluate.
+        layer_name (str): Name of the layer being evaluated.
+
+        Returns:
+        GeoDataFrame: Updated shade dataset with heat risk information.
+        """
         start_time = time.time()
 
-        join = gpd.sjoin(shade, self.heatrisk[['geometry', 'HI_TOTAAL_S']], how="inner", predicate='intersects')
+        join = gpd.sjoin(shade, self.heatrisk[['geometry', 'HI_TOTAAL_S']], how="inner", predicate='intersects', lsuffix='_left', rsuffix='_right')
         avg_hr = join.groupby('id').agg({'HI_TOTAAL_S': 'mean'}).reset_index()
         shade = shade.merge(avg_hr, on='id', how='left')
         shade['heat_rs'] = shade['HI_TOTAAL_S'].round(2).fillna(0)
@@ -161,9 +222,37 @@ class CoolEval:
         return shade
 
     def eval_pet(self, shade: gpd.GeoDataFrame, layer_name: str) -> gpd.GeoDataFrame:
+        """
+        Evaluate the Physiological Equivalent Temperature (PET) for each shaded area.
+
+        This function computes the average PET value from a raster dataset and assigns it to each shaded area.
+        It then classifies the PET values into a recommendation system.
+
+        Parameters:
+        shade (GeoDataFrame): The shade dataset to evaluate.
+        layer_name (str): Name of the layer being evaluated.
+
+        Returns:
+        GeoDataFrame: Updated shade dataset with PET evaluation and recommendations.
+        """
         start_time = time.time()
         with rasterio.open(self.pet) as src:
-            stats = zonal_stats(shade, self.pet, stats=['mean'], affine=src.transform)
+            affine = src.transform
+
+            def process_chunk(shade_chunk):
+                return zonal_stats(shade_chunk, self.pet, stats=['mean'], affine=affine)
+
+            # Split GeoDataFrame into smaller chunks to process in parallel
+            chunks = np.array_split(shade, 4)  # Change 4 depending on your system's CPU cores
+
+            # Use ThreadPoolExecutor for parallel processing
+            with ThreadPoolExecutor() as executor:
+                results = list(executor.map(process_chunk, chunks))
+
+        # Combine the results from all threads
+        stats = [stat for chunk_stats in results for stat in chunk_stats]
+
+        # Process the results
         avg_pet = [stat['mean'] for stat in stats]
         shade['PET'] = avg_pet
         shade['PET'] = shade['PET'].round()
@@ -176,8 +265,10 @@ class CoolEval:
 
     def aggregate_to_cool_places(self) -> None:
         """
-        After evaluating the shade geometries, aggregate the results to cool places
-        based on the common 'id' field between cool places and shade geometries.
+        Aggregate evaluation results from different shade layers to cool places.
+
+        This function collects and averages the evaluation results (e.g., capacity, benches, heat risk, PET)
+        from multiple shade geometry columns, then assigns the results back to the cool places dataset.
         """
         start_time = time.time()
         for i, eval_shade in enumerate(self.eval_shades):
@@ -193,19 +284,25 @@ class CoolEval:
             self.cool_places = self.cool_places.join(eval_shade_renamed[columns_to_add], how='left')
             print(f"Processed layer {i + 1} and added columns: {columns_to_add}")
 
-        columns_to_average = ['cap_area', 'cap_status,' 'benches_count', 'heat_rs', 'PET']
+        columns_to_average = ['cap_area', 'cap_status', 'benches_count', 'heat_rs', 'PET']
 
         for feature in columns_to_average:
             feature_columns = [col for col in self.cool_places.columns if re.match(f'{feature}_\d+$', col)]
             if feature_columns:
-                self.cool_places[feature_columns] = self.cool_places[feature_columns].apply(pd.to_numeric,
-                                                                                            errors='coerce')
+                self.cool_places[feature_columns] = self.cool_places[feature_columns].apply(pd.to_numeric,errors='coerce')
                 self.cool_places[f'{feature}_avg'] = self.cool_places[feature_columns].mean(axis=1)
+        print("Columns after agg:", self.cool_places.columns.tolist())
         end_time = time.time()
         duration = end_time - start_time
         print(f"Integrating attribute to cool space took {duration:.2f} seconds")
 
     def final_recom(self):
+        """
+        Calculate a final recommendation score for each cool place.
+
+        The function combines weighted scores from capacity, benches, heat risk, PET, and shade availability
+        to create a final recommendation ("Not recommended", "Recommended", "Highly Recommended") for each cool place.
+        """
         w_capacity = 0.2
         w_bench = 0.1
         w_hr = 0.2
@@ -249,13 +346,13 @@ class CoolEval:
         self.cool_places['scDay_c'] = self.cool_places['scDay'].apply(classify_shade).round(2)
 
         self.cool_places['score'] = ((self.cool_places['capst_norm'] * w_capacity) +
-                                     (self.cool_places['bc_norm'] * w_bench) +
-                                     (self.cool_places['hrs_norm'] * w_hr) +
-                                     (self.cool_places['PET_c'] * w_pet) +
-                                     (self.cool_places['scDay_c'] * w_shade)).round(2)
+                         (self.cool_places['bc_norm'] * w_bench) +
+                         (self.cool_places['hrs_norm'] * w_hr) +
+                         (self.cool_places['PET_c'] * w_pet) +
+                         (self.cool_places['scDay_c'] * w_shade)).round(2)
 
         def classify(row):
-            if row['scDay_c'] == 0 or row['PET_c'] == 0:  # If shade is 0, automatically "Not Recommended"
+            if row['scDay_c'] == 0 or row['PET_c'] == 0:
                 return 'Not recommended'
             elif row['score'] <= 0.4:
                 return 'Not recommended'
@@ -268,7 +365,14 @@ class CoolEval:
 
     def export_eval_gpkg(self, output_gpkg_path: str, layer_name: str) -> None:
         """
-        Export the evaluation results to a GeoPackage as a specific layer.
+        Export the evaluated cool places to a GeoPackage file.
+
+        This function prepares the data, including converting geometries to WKT format if necessary,
+        and saves the final evaluation results as a new layer in a GeoPackage.
+
+        Parameters:
+        output_gpkg_path (str): The file path for the output GeoPackage.
+        layer_name (str): The name of the layer to save in the GeoPackage.
         """
 
         print("Columns before dropping:", self.cool_places.columns.tolist())
@@ -283,11 +387,11 @@ class CoolEval:
         else:
             raise ValueError("The 'geometry' column is missing in the GeoDataFrame.")
 
+
         print("GeoDataFrame structure before export:")
         print(self.cool_places.head())
         print("Remaining columns after dropping:", self.cool_places.columns.tolist())
 
+
         self.cool_places.to_file(output_gpkg_path, layer=layer_name, driver='GPKG')
         print(f"Layer '{layer_name}' saved to GeoPackage: {output_gpkg_path}")
-
-
